71fa9a0ffe9909b0b7a1330c470c685ed02c4f1c
Rahulm9508 <153660317+Rahulm9508@users.noreply.github.com>
2025-02-01 12:22:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ except ValueError:
 ADMINS.append(OWNER)
 
 bot = Client("bot",    
-   bot_token="7451364068:AAHuhmL_f1XsLkMOSIIJSeGCIVB7_fqmicM",    
+   bot_token="7215328708:AAEepjJ1Z2Mns9KFEkD1-TpajR66rdMjeIY",    
    api_id= 20346550,    
    api_hash= "bc79c3bea7a626887bdc0871eecf0327"
 )
